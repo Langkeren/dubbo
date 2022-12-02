aa09700549a9a45a340ba5b9c2b93bc240beaf92
jingx <ljx20132200022@gmail.com>
2022-12-02 16:22:18 +0800

    doc: 服务导出-导出服务到远程 创建服务器实例 bind绑定地址

--- dubbo-remoting/dubbo-remoting-api/src/main/java/org/apache/dubbo/remoting/exchange/Exchangers.java
+++ dubbo-remoting/dubbo-remoting-api/src/main/java/org/apache/dubbo/remoting/exchange/Exchangers.java
@@ -69,6 +69,7 @@ public class Exchangers {
         // 获取 Exchanger，默认为 HeaderExchanger。
         // 紧接着调用 HeaderExchanger 的 bind 方法创建 ExchangeServer 实例
         url = url.addParameterIfAbsent(Constants.CODEC_KEY, "exchange");
+        // 获取 Exchanger，默认为 HeaderExchanger。
         return getExchanger(url).bind(url, handler);
     }
 
