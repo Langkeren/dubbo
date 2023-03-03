ae390a616fd29b18155bafaeeaf0cb65e135151d
jingx <ljx20132200022@gmail.com>
2023-03-03 12:05:39 +0800

    2.3 RoundRobinLoadBalance 版本

--- dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/loadbalance/RoundRobinLoadBalance.java
+++ dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/loadbalance/RoundRobinLoadBalance.java
@@ -36,8 +36,11 @@ public class RoundRobinLoadBalance extends AbstractLoadBalance {
     private static final int RECYCLE_PERIOD = 60000;
 
     protected static class WeightedRoundRobin {
+        // 服务提供者权重
         private int weight;
+        // 当前权重
         private AtomicLong current = new AtomicLong(0);
+        // 最后一次更新时间
         private long lastUpdate;
 
         public int getWeight() {
@@ -46,14 +49,17 @@ public class RoundRobinLoadBalance extends AbstractLoadBalance {
 
         public void setWeight(int weight) {
             this.weight = weight;
+            // 初始情况下，current = 0
             current.set(0);
         }
 
         public long increaseCurrent() {
+            // current = current + weight；
             return current.addAndGet(weight);
         }
 
         public void sel(int total) {
+            // current = current - total;
             current.addAndGet(-1 * total);
         }
 
@@ -66,6 +72,19 @@ public class RoundRobinLoadBalance extends AbstractLoadBalance {
         }
     }
 
+    // 嵌套 Map 结构，存储的数据结构示例如下：
+    // {
+    //     "UserService.query": {
+    //         "url1": WeightedRoundRobin@123,
+    //         "url2": WeightedRoundRobin@456,
+    //     },
+    //     "UserService.update": {
+    //         "url1": WeightedRoundRobin@123,
+    //         "url2": WeightedRoundRobin@456,
+    //     }
+    // }
+    // 最外层为服务类名 + 方法名，第二层为 url 到 WeightedRoundRobin 的映射关系。
+    // 这里我们可以将 url 看成是服务提供者的 id
     private ConcurrentMap<String, ConcurrentMap<String, WeightedRoundRobin>> methodWeightMap = new ConcurrentHashMap<String, ConcurrentMap<String, WeightedRoundRobin>>();
 
     /**
@@ -90,34 +109,54 @@ public class RoundRobinLoadBalance extends AbstractLoadBalance {
     protected <T> Invoker<T> doSelect(List<Invoker<T>> invokers, URL url, Invocation invocation) {
         // key = 全限定类名 + "." + 方法名，比如 com.xxx.DemoService.sayHello
         String key = invokers.get(0).getUrl().getServiceKey() + "." + invocation.getMethodName();
+        // 获取 url 到 WeightedRoundRobin 映射表，如果为空，则创建一个新的
         ConcurrentMap<String, WeightedRoundRobin> map = methodWeightMap.computeIfAbsent(key, k -> new ConcurrentHashMap<>());
         // 权重总和
         int totalWeight = 0;
         long maxCurrent = Long.MIN_VALUE;
+        // 获取当前时间
         long now = System.currentTimeMillis();
         Invoker<T> selectedInvoker = null;
         WeightedRoundRobin selectedWRR = null;
-        // 下面这个循环主要用于查找最大和最小权重，计算权重总和等
+
+        // 下面这个循环主要做了这样几件事情：
+        //   1. 遍历 Invoker 列表，检测当前 Invoker 是否有
+        //      相应的 WeightedRoundRobin，没有则创建
+        //   2. 检测 Invoker 权重是否发生了变化，若变化了，
+        //      则更新 WeightedRoundRobin 的 weight 字段
+        //   3. 让 current 字段加上自身权重，等价于 current += weight
+        //   4. 设置 lastUpdate 字段，即 lastUpdate = now
+        //   5. 寻找具有最大 current 的 Invoker，以及 Invoker 对应的 WeightedRoundRobin，
+        //      暂存起来，留作后用
+        //   6. 计算权重总和
         for (Invoker<T> invoker : invokers) {
             String identifyString = invoker.getUrl().toIdentityString();
             int weight = getWeight(invoker, invocation);
+            // 检测当前 Invoker 是否有对应的 WeightedRoundRobin，没有则创建
+            // 存储 url 唯一标识 identifyString 到 weightedRoundRobin 的映射关系
             WeightedRoundRobin weightedRoundRobin = map.computeIfAbsent(identifyString, k -> {
                 WeightedRoundRobin wrr = new WeightedRoundRobin();
                 wrr.setWeight(weight);
                 return wrr;
             });
 
+            // Invoker 权重不等于 WeightedRoundRobin 中保存的权重，说明权重变化了，此时进行更新
             if (weight != weightedRoundRobin.getWeight()) {
                 //weight changed
                 weightedRoundRobin.setWeight(weight);
             }
+            // 让 current 加上自身权重，等价于 current += weight
             long cur = weightedRoundRobin.increaseCurrent();
+            // 设置 lastUpdate，表示近期更新过
             weightedRoundRobin.setLastUpdate(now);
+            // 找出最大的 current
             if (cur > maxCurrent) {
                 maxCurrent = cur;
+                // 将具有最大 current 权重的 Invoker 赋值给 selectedInvoker
                 selectedInvoker = invoker;
                 selectedWRR = weightedRoundRobin;
             }
+            // 计算权重总和
             totalWeight += weight;
         }
         if (invokers.size() != map.size()) {
